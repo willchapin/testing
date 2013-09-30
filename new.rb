dde823130416f079e39cab69f3bda12716f9a587
willchapin <wrchapin@gmail.com>
2013-09-30 14:56:42 -0400

    fizzbuzz with prime numbers

--- new.rb
+++ new.rb
@@ -1,9 +1,9 @@
-(1..100).each do |num|
-  if num % 5 == 0 && num % 3 == 0
+(1..1000).each do |num|
+  if num % 7 == 0 && num % 3 == 0
     puts "buzzfizz"
-  elsif num % 5 == 0
+  elsif num % 11 == 0
     puts "buzz"
-  elsif num % 3 == 0
+  elsif num % 13 == 0
     puts "fizz"
   else
     puts num
